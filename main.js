7c7ebb00116097bc25a852ae2b24d812ec152074
ojasva2011 <90315077+ojasva2011@users.noreply.github.com>
2022-08-03 16:39:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+song1 = "";
+song2 = "";
+left_wristX = 0;
+left_wristY = 0;
+right_wristX = 0;
+right_wristY = 0;
+
+function preload(){
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+    
+}
+
+function modelLoaded(){
+    console.log("modelLoaded")
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log(" leftWristX =" + leftWristX + " leftWristY =" + leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log(" rightWristX = " + rightWristX + " rightWristY = " + rightWristY );
+    }
+}
